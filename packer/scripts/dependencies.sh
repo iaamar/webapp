3373b2b03a57e9dadd2c0787897ee1b339962996
Amar Nagargoje <amargithub@gmail.com>
2024-10-17 10:15:30 -0400

    aws cli config erorr fix

--- packer/scripts/dependencies.sh
+++ packer/scripts/dependencies.sh
@@ -12,29 +12,15 @@ sudo ./aws/install
 
 # Verify AWS CLI installation
 aws --version
-
-# Ensure that AWS credentials directory exists
-mkdir -p ~/.aws
-# Write the credentials to the AWS credentials file for the "dev" profile
-cat <<EOT >> ~/.aws/credentials
-[dev]
-aws_access_key_id=$AWS_ACCESS_KEY_ID
-aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
-EOT
-
-# Write the region to the AWS config file for the "dev" profile
-mkdir -p ~/.aws
-cat <<EOT >> ~/.aws/config
-[profile dev]
-region=$AWS_DEFAULT_REGION
-EOT
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile dev
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile dev
+aws configure set region $REGION --profile dev
+aws configure set output json --profile dev
+aws configure list --profile dev
 
 echo "AWS credentials and region have been configured successfully for the 'dev' profile."
 cat ~/.aws/credentials
 cat ~/.aws/config
-
-# Verify the AWS CLI is configured and working
-aws configure list --profile dev
 
 # Add NodeSource PPA for the latest stable version of Node.js (replace version as needed)
 curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash -
