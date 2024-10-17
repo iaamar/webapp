86279d876834edad057740d8deef9aaf5ed4c2cc
Amar Nagargoje <amargithub@gmail.com>
2024-10-17 10:45:46 -0400

    aws cli config erorr fix

--- packer/scripts/db-setup.sh
+++ packer/scripts/db-setup.sh
@@ -7,11 +7,4 @@ sudo -u postgres psql -c "CREATE USER amarnagargoje WITH PASSWORD 'user1234';"
 sudo -u postgres psql -c "CREATE DATABASE healthcare OWNER amarnagargoje;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE healthcare TO amarnagargoje;"
 
-sudo -u postgres psql -c "DROP DATABASE IF EXISTS ${process.env.DB_DATABASE};"
-sudo -u postgres psql -c "DROP ROLE IF EXISTS ${process.env.DB_USER};"
-sudo -u postgres psql -c "CREATE ROLE ${process.env.DB_USER} WITH LOGIN PASSWORD '${process.env.DB_PASSWORD}';"
-sudo -u postgres psql -c "CREATE DATABASE ${process.env.DB_DATABASE};"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${process.env.DB_DATABASE} TO ${process.env.DB_USER};"
-sudo -u postgres psql -c "ALTER ROLE ${process.env.DB_USER} WITH SUPERUSER;"
-
 echo "Database setup successfully."
