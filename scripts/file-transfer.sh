28b297d0cdbe36ad12e7d077246dfe685ee72f72
Amar Nagargoje <amargithub@gmail.com>
2024-10-19 12:10:31 -0400

    user acces to webapp

--- scripts/file-transfer.sh
+++ scripts/file-transfer.sh
@@ -26,6 +26,9 @@ sudo mv /tmp/webapp.zip /opt
 sudo mv /tmp/mywebapp.service /etc/systemd/system
 
 # Change ownership of /opt/webapp to the 'csye6225' user
+#unzip the file
+sudo unzip /opt/webapp.zip -d /opt/webapp
+ls -a /opt/webapp
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chmod 755 /opt/webapp
 
@@ -35,7 +38,4 @@ sudo chmod 600 /opt/webapp/.env
 sudo chown csye6225:csye6225 /etc/systemd/system/mywebapp.service
 sudo chmod 644 /etc/systemd/system/mywebapp.service
 
-# Unzip the webapp.zip file into /opt/webapp
-sudo unzip /opt/webapp.zip -d /opt/webapp
-ls -a /opt/webapp
 echo "File transfer and setup completed successfully."
