df40e37fd078dafa4ce701bc76a98648b804dccb
Amar Nagargoje <amargithub@gmail.com>
2024-10-19 12:05:47 -0400

    adding user with nologin

--- scripts/file-transfer.sh
+++ scripts/file-transfer.sh
@@ -2,10 +2,18 @@
 
 # Set the -e option to exit on any error
 set -e
-# Create the 'csye6225' user and group if not already exists
+# Create the 'csye6225' group if it doesn't exist
+if ! getent group csye6225 &>/dev/null; then
+  sudo groupadd csye6225
+  echo "Group 'csye6225' created successfully."
+else
+  echo "Group 'csye6225' already exists."
+fi
+
+# Create the 'csye6225' user with primary group 'csye6225' and set /usr/sbin/nologin as the shell
 if ! id "csye6225" &>/dev/null; then
-  sudo adduser --system --no-create-home --shell /usr/sbin/nologin csye6225
-  echo "User 'csye6225' created successfully."
+  sudo useradd -r -g csye6225 -s /usr/sbin/nologin csye6225
+  echo "User 'csye6225' created successfully with /usr/sbin/nologin as the shell."
 else
   echo "User 'csye6225' already exists."
 fi
