280f61a2d740b1294e4c466c5799d2b6226058a3
Amar Nagargoje <amargithub@gmail.com>
2024-10-22 21:33:59 -0400

    a05 setup-removed postgres setup

--- scripts/file-transfer.sh
+++ scripts/file-transfer.sh
@@ -9,38 +9,6 @@ sudo unzip -o /tmp/webapp.zip -d /opt/webapp
 cd /opt/webapp
 ls -la
 
-# Check if .env file exists after extraction
-if [ -f ".env" ]; then
-  echo ".env file found:"
-  cat .env  # Display the contents of the .env file
-else
-  echo ".env file not found. Creating a new .env file with default variables."
-
-  # Create a new .env file with specified variables
-sudo bash -c 'cat <<EOF > /opt/webapp/.env
-DB_HOST=localhost
-DB_PORT=5432
-DB_USER=amarnagargoje
-DB_PASSWORD=user1234
-DB_DATABASE=healthcare
-AWS_ACCESS_KEY_ID="AKIAY6QVZHUMDQKGOLGR"
-AWS_SECRET_ACCESS_KEY="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"
-AWS_DEFAULT_REGION="us-east-2"
-AWS_OUTPUT_FORMAT="json"
-EOF'
-  echo ".env file created successfully."
-  echo ".env file contents:"
-  cat .env  # Display the contents of the .env file
-fi
-
-# Set permissions for .env and systemd service files
-if [ -f ".env" ]; then
-  sudo chown csye6225:csye6225 /opt/webapp/.env
-  sudo chmod 600 /opt/webapp/.env
-else
-  echo "Skipping .env file permissions setup: file not found."
-fi
-
 # Set correct ownership
 sudo chown -R csye6225:csye6225 /opt/webapp
 
