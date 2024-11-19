3f522c73889dfadc901992ed8d86812f4556f60a
Amar Nagargoje <amargithub@gmail.com>
2024-11-18 22:50:16 -0500

    test workflow

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,30 +1,45 @@
 import request from "supertest";
 import { app } from "../src/index";
 import sequelize, { bootstrapDatabase } from "../src/database/connect";
-import { User } from "../src/models/User";
+
 
 let server: any;
+let port;
+
 beforeAll(async () => {
-  await bootstrapDatabase();
-  server = app.listen(9001);
+  await bootstrapDatabase(); // Bootstrap the database
 });
 
-describe("Test Suite", () => {
-  test("Health Check API", async () => {
-    const res = await request(app).get("/healthz");
+afterAll(async () => {
+  if (server) {
+    server.close(); // Close the server to free resources
+  }
+  await sequelize.close(); // Close the database connection
+});
+
+describe("Test 1. Integration test for healthz api", () => {
+  server = app.listen(0); // Use dynamic port assignment
+  port = server.address().port; // Get the dynamically assigned port
+  
+  test('Test healthz route', async () => {
+    const res = await request(app).get(`/healthz`);
     expect(res.statusCode).toEqual(200);
   });
+});
 
-  test("Create User", async () => {
-    const user = {
-      email: "test@example.com",
-      password: "password",
-      first_name: "Test",
-      last_name: "User",
+describe("POST /v1/user - Create a new user", () => {
+  server = app.listen(0); // Use dynamic port assignment
+  port = server.address().port; // Get the dynamically assigned port
+  
+  it("should create a new user with valid input", async () => {
+    const dummyUser = {
+      email: "ama@gmail.com",
+      password: "amar",
+      first_name: "Amar",
+      last_name: "Nagargoje",
     };
 
-    const res = await request(app).post("/v1/user").send(user);
-    expect(res.statusCode).toBe(201);
-    expect(res.body.email).toBe(user.email);
+    const response = await request(app).post("/v1/user").send(dummyUser);
+    expect(response.status).toBe(201);
   });
 });
